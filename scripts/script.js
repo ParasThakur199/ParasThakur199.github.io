b3d10b6c89c86118770633b03ec8e85a556d9447
ParasThakur199 <parasjamwal19@gmail.com>
2023-02-04 19:46:02 +0530

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+function Shownav(){
+    document.getElementById("mob-links").style.display="flex";
+}
+function Closenav(){
+    document.getElementById("mob-links").style.display="none";
+}
+
+function redirect(url){
+    window.open(url);
+}
+function download(url){
+    window.location.href=url;
+    window.open('https://drive.google.com/file/d/1r7NUifshN3IWZ3_a_tDkj8rz72DL4zRC/view');
+}
